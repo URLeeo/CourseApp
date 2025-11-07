e11501cd4be68dd863cf60cd4fb2f88bcfa960b5
URLeeo <mammadzadaaslan05@gmail.com>
2025-11-07 22:35:06 +0400

    Created Onion Arc and Folders

---
+++ CourseApp/CourseApp.Domain/Models/Group.cs
@@ -0,0 +1,9 @@
+﻿using CourseApp.Domain.Models.Shared;
+
+namespace CourseApp.Domain.Models;
+
+public class Group : BaseEntity
+{
+    public string Teacher { get; set; }
+    public string Room { get; set; }
+}
---
+++ CourseApp/CourseApp.Domain/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using CourseApp.Domain.Models.Shared;
+
+namespace CourseApp.Domain.Models;
+
+public class Student : BaseEntity
+{
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public Group Group { get; set; }
+}
